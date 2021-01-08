9b05b1b0a743ab7bfa03ef173108cf41bd6f4e00
sambodjango <gringolalia@gmail.com>
2021-01-08 16:45:42 -0300

    c4fcd36

---
+++ js/12.3a75562b.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[12],{6234:function(n,e,t){"use strict";t.r(e);var a=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("q-page",{attrs:{padding:""}})},l=[],r={},s=r,u=t("2877"),c=Object(u["a"])(s,a,l,!1,null,null,null);e["default"]=c.exports}}]);
